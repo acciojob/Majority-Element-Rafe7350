6651d9c7df89076d0eb1cf7c20b7509ee9d4dcd9
Rafe7350 <117887198+Rafe7350@users.noreply.github.com>
2023-03-17 19:12:30 +0530

    add

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your code here
+function majorityElement(nums) {
+    const counts = {};
+    const threshold = Math.floor(nums.length / 2);
+    for (let i = 0; i < nums.length; i++) {
+      const num = nums[i];
+      counts[num] = (counts[num] || 0) + 1;
+      if (counts[num] > threshold) {
+        return num;
+      }
+    }
+  }
+  
